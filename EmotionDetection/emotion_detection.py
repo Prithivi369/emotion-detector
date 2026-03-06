90f2c708198779e05c2ffb1205d93d3cca8ec940
Prithivi Alamyan <prithivialamyan@Prithivis-MacBook-Air.local>
2026-03-06 13:06:16 +0530

    IBM Emotion Detector Project

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,11 @@
+def emotion_detector(text_to_analyze):
+
+    emotions = {
+        "anger": 0.02,
+        "disgust": 0.01,
+        "fear": 0.03,
+        "joy": 0.88,
+        "sadness": 0.06
+    }
+
+    return emotions
